38b38463117b18923e4098cc06289964bc1b639f
Bryan Rodriguez <bryan.rodriguez39@upr.edu>
2021-12-01 14:34:19 -0400

    base

--- db/php_project_db.sql
+++ db/php_project_db.sql
@@ -1,35 +1,322 @@
-CREATE TABLE course (
-	course_id	CHAR(8) NOT NULL,
-	title	VARCHAR(100) NOT NULL,
-	credits	TINYINT(1) UNSIGNED NOT NULL,
-	PRIMARY KEY (course_id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2021 at 07:33 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
 
-CREATE TABLE section (
-	course_id	CHAR(8) NOT NULL,
-	section_id	CHAR(3) NOT NULL,
-	capacity	TINYINT(1) UNSIGNED NOT NULL,
-	PRIMARY KEY (course_id, section_id),
-	FOREIGN KEY (course_id)
-		REFERENCES course (course_id) ON DELETE NO ACTION
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE student (
-	student_id	VARCHAR(30) NOT NULL,
-	password	VARCHAR(255) NOT NULL,
-	name	VARCHAR(50),
-	year_of_study	TINYINT(1) UNSIGNED NOT NULL,
-	PRIMARY KEY (student_id)
-);
 
-CREATE TABLE enrollments (
-	student_id	VARCHAR(30) NOT NULL,
-	course_id	CHAR(8) NOT NULL,
-	section_id	CHAR(3) NOT NULL,
-	status	TINYINT(1) UNSIGNED NOT NULL,
-	PRIMARY KEY (student_id, course_id),
-	FOREIGN KEY (student_id)
-		REFERENCES student (student_id) ON DELETE NO ACTION,
-	FOREIGN KEY (course_id, section_id)
-		REFERENCES section (course_id, section_id) ON DELETE NO ACTION
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `creationDate`, `updationDate`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', '2020-01-24 16:21:18', '01-12-2021 03:46:30 AM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` char(8) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `credits` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `title`, `credits`) VALUES
+('CCOM3001', 'Intro A Prog.', 2),
+('CCOM4019', 'Curso de PHP ', 2),
+('CIBI3001', 'Intro a Biology', 4),
+('FISI3001', 'Intro to Physics', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `department`, `creationDate`) VALUES
+(1, 'CCOM', '2021-11-30 20:39:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `student_id` varchar(30) NOT NULL,
+  `course_id` char(8) NOT NULL,
+  `section_id` char(3) NOT NULL,
+  `status` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `course_id` char(8) NOT NULL,
+  `section_id` char(3) NOT NULL,
+  `capacity` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`course_id`, `section_id`, `capacity`) VALUES
+('CCOM3001', 'ME5', 30),
+('CCOM4019', 'L10', 5),
+('CIBI3001', 'M10', 5),
+('FISI3001', 'L10', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semester`
+--
+
+CREATE TABLE `semester` (
+  `id` int(11) NOT NULL,
+  `semester` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `semester`
+--
+
+INSERT INTO `semester` (`id`, `semester`, `creationDate`, `updationDate`) VALUES
+(1, 'C12', '2021-11-30 20:39:54', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE `session` (
+  `id` int(11) NOT NULL,
+  `session` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `session`
+--
+
+INSERT INTO `session` (`id`, `session`, `creationDate`) VALUES
+(1, 'This is a Test 1', '2021-11-30 20:39:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `year_of_study` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `password`, `name`, `year_of_study`) VALUES
+('elian.acevedo', '25d55ad283aa400af464c76d713c07ad', 'Elian', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(255) NOT NULL,
+  `userip` binary(16) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `student_id`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(13, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 15:28:35', '', 1),
+(14, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 15:31:08', '', 1),
+(15, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 15:35:20', '01-12-2021 09:09:12 PM', 1),
+(16, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 15:39:18', '01-12-2021 09:10:43 PM', 1),
+(17, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 17:05:10', '', 1),
+(18, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 18:23:02', '01-12-2021 11:56:02 PM', 1),
+(19, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 18:26:27', '01-12-2021 11:56:41 PM', 1),
+(20, 'elian.acevedo', 0x3a3a3100000000000000000000000000, '2021-12-01 18:29:12', '', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`student_id`,`course_id`),
+  ADD KEY `course_id` (`course_id`,`section_id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`course_id`,`section_id`);
+
+--
+-- Indexes for table `semester`
+--
+ALTER TABLE `semester`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `session`
+--
+ALTER TABLE `session`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `semester`
+--
+ALTER TABLE `semester`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `session`
+--
+ALTER TABLE `session`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`,`section_id`) REFERENCES `section` (`course_id`, `section_id`) ON DELETE NO ACTION;
+
+--
+-- Constraints for table `section`
+--
+ALTER TABLE `section`
+  ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
